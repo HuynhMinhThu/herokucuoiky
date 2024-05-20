cb6854781356d8d57a39af6df19177f40902dbdc
HuynhMinhThu <157386347+HuynhMinhThu@users.noreply.github.com>
2024-05-21 00:38:02 +0700

    Update connectDB.php

--- PHP-Bookstore-Website-Example-master/bookstore/connectDB.php
+++ PHP-Bookstore-Website-Example-master/bookstore/connectDB.php
@@ -1,3 +1,3 @@
 <?php
-$pdo=new PDO('mysql:host=us-cluster-east-01.k8s.cleardb.net;port=3306;dbname=heroku_396a2b66c8693eb','root', '');
+$pdo=new PDO('mysql:host=us-cluster-east-01.k8s.cleardb.net;port=3306;dbname=heroku_396a2b66c8693eb','bc8d73174f440f', '7215f4fc');
 ?>
